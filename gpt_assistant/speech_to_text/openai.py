d488bfbccf9cc0dcac4412944eac7ed9fdea7641
Jaron Zhou <jaronzhou25030@gmail.com>
2024-04-01 22:11:36 -0500

    Initial commit

---
+++ gpt_assistant/speech_to_text/openai.py
@@ -0,0 +1,11 @@
+import os
+import openai
+
+//add the key
+
+def transcribe_audio(filename):
+    """Transcribe audio using OpenAI's Whisper"""
+    file = open(filename, "rb")
+    transcript = openai.Audio.transcribe("whisper-1", file=file, language=os.getenv("OPENAI_WHISPER_LANGUAGE", "en"))
+    print("\rYou:", transcript.text)
+    return transcript.text
